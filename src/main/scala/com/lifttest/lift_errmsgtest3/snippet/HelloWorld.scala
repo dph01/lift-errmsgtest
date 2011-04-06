839d1986e6c0ebf1b955b9575d6c6de1b88f1f42
Damian H <me@damianhelme.com>
2011-04-06 16:03:25 +0100

    adding the rest of the contents to the first commit

---
+++ src/main/scala/com/lifttest/lift_errmsgtest3/snippet/HelloWorld.scala
@@ -0,0 +1,44 @@
+package com.lifttest.lift_errmsgtest3 {
+package snippet {
+
+import _root_.scala.xml.{NodeSeq, Text}
+import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
+import _root_.java.util.Date
+import net.liftweb.http.{StatefulSnippet,S, SHtml, DispatchSnippet, RequestVar, SessionVar}
+import net.liftweb.http.js.JsCmds.{RedirectTo,SetHtml,Noop}
+import com.lifttest.lift_errmsgtest3.lib._
+import Helpers._
+
+class HelloWorld {
+                                                   
+  var name = ""
+	  
+  def updateResults() = {
+	println("in updateResults, name = " + name)
+	SetHtml("result", if (name != "" ) Text("Hello " + name) else Text("") )
+  }
+  
+  def setName(n: String) {
+	println("in setName, name = " + n)
+	if (n == "" ) {
+		S.error("Name cannot be blank!")
+	} else {
+		S.clearCurrentNotices
+	}
+	name = n
+  }
+  def howdy(in: NodeSeq): NodeSeq = {
+   	SHtml.ajaxForm(
+		Helpers.bind("b", in, 
+			"name" -> SHtml.text(name, setName(_)),
+			"submit" -> SHtml.ajaxSubmit("Go", updateResults),
+			"result" -> Text("")
+		)
+	)
+  }
+
+}
+
+}
+}
